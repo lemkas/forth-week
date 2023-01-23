0637206fa471e40209f1f6894b43781fd1552fbf
lemov nikita <lemov1980@yandex.ru>
2023-01-24 02:21:06 +0300

    configured disabling checkbox

--- src/app/components/feedback-form/feedback-form.component.ts
+++ src/app/components/feedback-form/feedback-form.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { IFeedbackForm } from 'src/app/models/feedback-form';
-import { CheckboxValidator } from 'src/app/validators/checkbox.validator';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
@@ -12,24 +18,61 @@ import { ModalComponent } from '../modal/modal.component';
 })
 export class FeedbackFormComponent implements OnInit {
   feedBackForm!: FormGroup;
-  constructor(
-    private fb: FormBuilder,
-    private dialog: MatDialog,
-    private checkboxValidator: CheckboxValidator
-  ) {}
+  isValidFields: boolean = false;
+  subscription!: Subscription;
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.initForm();
-    console.log(this.feedBackForm.get('fio'));
+    this.checkValid();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
   initForm() {
     this.feedBackForm = this.fb.group({
-      fio: ['', [Validators.required, Validators.min(6), Validators.max(256)]],
-      email: ['', [Validators.required, Validators.email, Validators.max(256)]],
+      fio: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(256),
+        ],
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.maxLength(256)],
+      ],
       phoneNumber: ['+7', Validators.required],
-      comment: ['', Validators.max(256)],
-      consent: ['', Validators.requiredTrue],
+      comment: ['', Validators.maxLength(256)],
+      consent: [false, Validators.requiredTrue],
+    });
+  }
+
+  checkValid() {
+    // this.subscription = this.feedBackForm.statusChanges.subscribe((status) => {
+    //   if (status === 'VALID') {
+    //     this.isValidFields = true;
+    //     console.log(status);
+    //   } else if (status === 'INVALID') {
+    //     this.isValidFields = false;
+    //     console.log(status);
+    //   }
+    // });
+
+    this.subscription = this.feedBackForm.valueChanges.subscribe((data) => {
+      const fio = this.feedBackForm.get('fio');
+      const email = this.feedBackForm.get('email');
+      const phoneNumber = this.feedBackForm.get('phoneNumber');
+
+      if (fio?.valid && email?.valid && phoneNumber?.valid) {
+        this.isValidFields = true;
+        console.log(this.isValidFields);
+      } else {
+        this.isValidFields = false;
+        console.log(this.isValidFields);
+      }
     });
   }
 
